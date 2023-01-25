298aee519e21ae0b9c679f3ff93dbebcbf2cfebe
karoliks <karoline.k.sabo@gmail.com>
2023-01-25 19:09:30 +0100

    Include conflicts to the EF1 calculations

--- sat/src/sat.py
+++ sat/src/sat.py
@@ -36,13 +36,73 @@ def is_ef1_possible(n, m, V):
 
             # Check that there is no envy once an item is possibly dropped
             s.add(Sum([If(A[i][g], 1, 0) * V[i][g] for g in range(m)]) >= 
-                  Sum([V[j][g] * (If(A[j][g], 1, 0) - If(D[i][j][g], 1, 0)) for g in range(m)]))
+                  Sum([V[j][g] * (If(A[j][g], 1, 0) - If(D[j][i][g], 1, 0)) for g in range(m)]))  # TODO dobbelsjekk omindeksenei D er riktige
 
-    print(s.check())
-    if(s.check() == sat):
-        print(s.model())
-        
+    # print(s.check())
+    # if(s.check() == sat):
+    #     print(s.model())
+
     return s.check() == sat
 
 
-# TODO: legge inn konflikter
+def is_ef1_with_conflicts_possible(n, m, V, G):
+    s = Solver()
+
+    # Make sure that the number of nodes in the graph matches the number of items and the valuation function
+    assert m == G.vcount(), "The number of items do not match the size of the graph"
+    assert m == V[0].size, "The number of items do not match the valuation function"
+
+    # A  keeps track of the allocated items
+    A = [[Bool("a_%s_%s" % (i, j)) for j in range(m)]
+         for i in range(n)]
+
+    # D keepstrack of items that are dropped when checking for Ef1
+    D = [[[Bool("d_%s_%s_%s" % (i, j, k)) for i in range(m)]
+          for j in range(n)] for k in range(n)]
+
+    # Each item allocated to at exactly one agent
+    for g in range(m):
+        # TODO double check that this is correct order
+        s.add(Sum([If(A[i][g], 1, 0) for i in range(n)]) <= 1)
+
+    for i in range(n):
+        for j in range(n):
+            # Make sure that when checking for EF1, each agent is only allowed togive away one item to make another agent not jealous
+            # TODO double check that this is correct order
+            s.add(Sum([If(D[i][j][g], 1, 0) for g in range(n)]) == 1)
+
+            for g in range(m):
+                # Make sure only items that are actuallallocated to the agent in question, is dropped
+                s.add(If(D[i][j][g], 1, 0) <= If(A[j][g], 1, 0))
+
+    for i in range(n):
+        for j in range(n):
+
+            if i == j:
+                continue
+            print(V[i][0])
+            # Check that there is no envy once an item is possibly dropped
+            s.add(Sum([V[i][g] * If(A[i][g], 1, 0)
+                       for g in range(m)]) >=
+                  Sum([V[i][g] * (If(A[j][g], 1, 0) - If(D[j][i][g], 1, 0))  # TODO dobbelsjekk omindeksenei D er riktige
+                       for g in range(m)]))
+
+    print(G.get_edgelist())
+    # Enforce the conflicts from the graph
+    for e in G.get_edgelist():
+        print(e)
+        print(e[0])
+        print(e[1])
+        g = e[0]
+        h = e[1]
+
+        for i in range(n):
+            # Make sure that a single agent only can get one of two conflicting items
+            s.add((If(A[i][g], 1, 0) + If(A[i][h], 1, 0)) <= 1)
+
+    # Check if an EF1 allocation can be found
+    print(s.check())
+    if(s.check() == sat):
+        print(s.model())
+
+    return s.check() == sat
--- sat/src/test_sat.py
+++ sat/src/test_sat.py
@@ -1,6 +1,7 @@
 import numpy as np
+from igraph import *
 
-from sat import is_ef1_possible
+from sat import is_ef1_possible, is_ef1_with_conflicts_possible
 
 
 def test_sum():
@@ -13,10 +14,25 @@ def test_ef1_no_conflicts():
     V = np.ones((n, m))
 
     assert is_ef1_possible(
-        n, m, V) == True, "EF1 should be possible when there are no conlficts"
+        n, m, V) == True, "EF1 should be possible when there are no conflicts"
+
+
+def test_ef1_with_conflicts():
+    n = 2
+    m = 3
+    # V = np.ones((n, m))
+    V = np.array([[1., 0., 1.], [1., 0., 1.]])
+    # G = ig.Graph.Ring(n=3, circular=False)
+    path = Graph.Ring(n=3, circular=False)
+    plot(path, target='path.pdf')
+    # path.get_edgelist()
+    print(V)
+    assert is_ef1_with_conflicts_possible(
+        n, m, V, path) == False, "EF1 should not be possible in this case"
 
 
 if __name__ == "__main__":
     test_sum()
     test_ef1_no_conflicts()
+    test_ef1_with_conflicts()
     print("Everything passed")
