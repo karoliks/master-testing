472f74532a05c2f2053ce7dad3b6ab10b5552425
karoliks <karoline.k.sabo@gmail.com>
2023-01-26 11:16:56 +0100

    Add test

--- sat/src/test_sat.py
+++ sat/src/test_sat.py
@@ -21,14 +21,16 @@ def test_ef1_with_conflicts():
     n = 2
     m = 3
     # V = np.ones((n, m))
-    V = np.array([[1., 0., 1.], [1., 0., 1.]])
+    V_1 = np.array([[1., 0., 1.], [1., 0., 1.]])
+    V_2 = np.array([[1., 1., 1.], [1., 1., 1.]])
     # G = ig.Graph.Ring(n=3, circular=False)
     path = Graph.Ring(n=3, circular=False)
     plot(path, target='path.pdf')
     # path.get_edgelist()
-    print(V)
     assert is_ef1_with_conflicts_possible(
-        n, m, V, path) == False, "EF1 should not be possible in this case"
+        n, m, V_1, path) == False, "EF1 should not be possible in this case"
+    assert is_ef1_with_conflicts_possible(
+        n, m, V_2, path) == True, "EF1 should be possible in this case"
 
 
 if __name__ == "__main__":
