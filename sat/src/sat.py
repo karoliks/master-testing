fe5625e45e3f5bea94053fb50791c59dd6f872a1
karoliks <karoline.k.sabo@gmail.com>
2023-01-30 14:29:40 +0100

    Refactor duplicate code

--- sat/src/sat.py
+++ sat/src/sat.py
@@ -2,6 +2,77 @@ from z3 import *
 import numpy as np
 
 
+### Helper functions ###
+########################
+
+def max_in_product_array(d_i_j, v_i):
+    product = [a*b for a, b in zip(d_i_j, v_i)]
+    m = product[0]
+    for v in product[1:]:
+        m = If(v > m, v, m)
+    return m
+
+
+def get_edge_conflicts(G, A, n):
+    conflicts = []
+    # Enforce the conflicts from the graph
+    for e in G.get_edgelist():
+        g = e[0]
+        h = e[1]
+
+        for i in range(n):
+            # Make sure that a single agent only can get one of two conflicting items
+            conflicts.append((If(A[i][g], 1, 0) + If(A[i][h], 1, 0)) <= 1)
+
+    return And([conflict for conflict in conflicts])
+
+
+def get_formula_for_correct_removing_of_items(A, D, n, m):
+    formulas = []
+
+    for i in range(n):
+        for j in range(n):
+            # Make sure that when checking for EF1, each agent is only allowed togive away one item to make another agent not jealous
+            # TODO double check that this is correct order
+            formulas.append(Sum([If(D[i][j][g], 1, 0) for g in range(n)]) == 1)
+
+            for g in range(m):
+                # Make sure only items that are actuallallocated to the agent in question, is dropped
+                formulas.append(If(D[i][j][g], 1, 0) <= If(A[j][g], 1, 0))
+
+    return And([formula for formula in formulas])
+
+
+def get_formula_for_one_item_for_each_agent(A, n, m):
+    formulas = []
+    # Each item allocated to at exactly one agent
+    for g in range(m):
+        # TODO double check that this is correct order
+        # var <= her. skal det ikke være ==?
+        formulas.append(Sum([If(A[i][g], 1, 0) for i in range(n)]) == 1)
+
+    return And([formula for formula in formulas])
+
+
+def get_formula_for_ensuring_ef1(A, D, V, n, m):
+    formulas = []
+
+    for i in range(n):
+        for j in range(n):
+
+            if i == j:
+                continue
+
+            # Check that there is no envy once an item is possibly dropped
+            formulas.append(Sum([If(A[i][g], 1, 0) * V[i][g] for g in range(m)]) >=
+                            Sum([V[j][g] * (If(A[j][g], 1, 0) - If(D[j][i][g], 1, 0)) for g in range(m)]))  # TODO dobbelsjekk omindeksenei D er riktige
+
+    return And([formula for formula in formulas])
+
+
+################################################################
+
+
 def is_ef1_possible(n, m, V):
     s = Solver()
 
@@ -13,30 +84,9 @@ def is_ef1_possible(n, m, V):
     D = [[[Bool("d_%s_%s_%s" % (i+1, j+1, k+1)) for i in range(m)]
           for j in range(n)] for k in range(n)]
 
-    # Each item allocated to at exactly one agent
-    for g in range(m):
-        # TODO double check that this is correct order
-        s.add(Sum([If(A[i][g], 1, 0) for i in range(n)]) == 1)
-
-    for i in range(n):
-        for j in range(n):
-            # Make sure that when checking for EF1, each agent is only allowed togive away one item to make another agent not jealous
-            # TODO double check that this is correct order
-            s.add(Sum([If(D[i][j][g], 1, 0) for g in range(n)]) == 1)
-
-            for g in range(m):
-                # Make sure only items that are actuallallocated to the agent in question, is dropped
-                s.add(If(D[i][j][g], 1, 0) <= If(A[j][g], 1, 0))
-
-    for i in range(n):
-        for j in range(n):
-
-            if i == j:
-                continue
-
-            # Check that there is no envy once an item is possibly dropped
-            s.add(Sum([If(A[i][g], 1, 0) * V[i][g] for g in range(m)]) >= 
-                  Sum([V[j][g] * (If(A[j][g], 1, 0) - If(D[j][i][g], 1, 0)) for g in range(m)]))  # TODO dobbelsjekk omindeksenei D er riktige
+    s.add(get_formula_for_one_item_for_each_agent(A, n, m))
+    s.add(get_formula_for_correct_removing_of_items(A, D, n, m))
+    s.add(get_formula_for_ensuring_ef1(A, D, V, n, m))
 
     return s.check() == sat
 
@@ -56,11 +106,14 @@ def is_ef1_with_conflicts_possible(n, m, V, G):
     D = [[[Bool("d_%s_%s_%s" % (i, j, k)) for i in range(m)]
           for j in range(n)] for k in range(n)]
 
-    # Each item allocated to at exactly one agent
-    for g in range(m):
-        # TODO double check that this is correct order
-        # var <= her. skal det ikke være ==?
-        s.add(Sum([If(A[i][g], 1, 0) for i in range(n)]) == 1)
+    s.add(get_formula_for_one_item_for_each_agent(A, n, m))
+    s.add(get_formula_for_correct_removing_of_items(A, D, n, m))
+    s.add(get_formula_for_ensuring_ef1(A, D, V, n, m))
+    s.add(get_edge_conflicts(G, A, n))
+
+    return s.check() == sat
+
+
 
     for i in range(n):
         for j in range(n):
