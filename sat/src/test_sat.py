3e8348086326f8dcbe2869a16ad4dd2f91d83df9
Karoline Sæbø <49613887+karoliks@users.noreply.github.com>
2023-02-16 10:42:07 +0100

    Discover valuation functions for a problem instance such that an allocation that satisfies EF1 is not possible (#1)
    * Add test
    
    * Change mistake if writing <= instzead of ==
    
    - This is used to make sure that all the items are allocated.
    - Earlier it was not guaranteed that all items were allocated
    - It also makes sure that an item is given to maximum one agent
    
    * Remove print statements
    
    * Refactor out edge computation
    
    * Refactor duplicate code
    
    * Begin to test out solution approaches
    
    The code does not behave the way it should atm
    
    * Fix bug due to == instead of <=
    
    * WIP: Expand lists and try Implies
    
     (still does not work)
    
    * WIP
    
    * Make finding valuation functions with now EF1 work
    
    Only tested on one specific test case atm
    
    * Test mistake removing
    
    * Change file structure
    
    * Refactoring
    
    * Remove trial and error file
    
    * Refactoring
    
    * Refactoring
    
    * Remove UnPack
    
    * Regret removing UnPack

--- sat/src/test_sat.py
+++ sat/src/test_sat.py
@@ -1,14 +1,14 @@
 import numpy as np
 from igraph import *
 
-from sat import is_ef1_possible, is_ef1_with_conflicts_possible
+from sat import find_valuation_function_with_no_ef1, is_ef1_possible, is_ef1_with_conflicts_possible
 
 
 def test_sum():
     assert sum([1, 2, 3]) == 6, "Should be 6"
 
 
-def test_ef1_no_conflicts():
+def test_ef1_no_conflicts_1():
     n = 2
     m = 3
     V = np.ones((n, m))
@@ -17,22 +17,62 @@ def test_ef1_no_conflicts():
         n, m, V) == True, "EF1 should be possible when there are no conflicts"
 
 
+def test_ef1_no_conflicts_2():
+    n = 7
+    m = 2
+    V = np.random.rand(n, m)
+
+    assert is_ef1_possible(
+        n, m, V) == True, "EF1 should be possible when there are no conflicts"
+
+
 def test_ef1_with_conflicts():
     n = 2
     m = 3
-    # V = np.ones((n, m))
-    V = np.array([[1., 0., 1.], [1., 0., 1.]])
-    # G = ig.Graph.Ring(n=3, circular=False)
+
+    V_1 = np.array([[1., 0., 1.], [1., 0., 1.]])
+    V_2 = np.array([[1., 1., 1.], [1., 1., 1.]])
+
     path = Graph.Ring(n=3, circular=False)
     plot(path, target='path.pdf')
-    # path.get_edgelist()
-    print(V)
+
     assert is_ef1_with_conflicts_possible(
-        n, m, V, path) == False, "EF1 should not be possible in this case"
+        n, m, V_1, path) == False, "EF1 should not be possible in this case"
+    assert is_ef1_with_conflicts_possible(
+        n, m, V_2, path) == True, "EF1 should be possible in this case"
+
+
+def test_discover_bad_valuation_functions():
+    n = 2
+    m = 3
+
+    path = Graph.Ring(n=3, circular=False)
+    plot(path, target='path.pdf')
+
+    assert find_valuation_function_with_no_ef1(
+        n, m, path)[0] == True, "Could not find a desired valuation function"
+
+
+def test_send_valuations_for_checking():
+    n = 2
+    m = 3
+
+    path = Graph.Ring(n=3, circular=False)
+    plot(path, target='path.pdf')
+
+    V = find_valuation_function_with_no_ef1(
+        n, m, path)[1]
+    V = np.array([V[0:3], V[3:6]])
+
+    assert is_ef1_with_conflicts_possible(
+        n, m, V, path) == False, "The program was not able to discover a set of valuation functions were EF1 is not possible"
 
 
 if __name__ == "__main__":
     test_sum()
-    test_ef1_no_conflicts()
+    test_ef1_no_conflicts_1()
+    test_ef1_no_conflicts_2()
     test_ef1_with_conflicts()
+    test_discover_bad_valuation_functions()
+    test_send_valuations_for_checking()
     print("Everything passed")
